39573056d654bb832b8590d774a873bae1262dd3
Oleksandr Bilousov <bilousov757@gmail.com>
2025-10-07 15:01:04 +0200

    Create new file auf5.04

---
+++ auf5.04/Erfolg.java
@@ -0,0 +1,21 @@
+// u05_kontrollstruktur
+// auf5.04
+
+public class Erfolg
+{
+   public static void main(String[] args)
+      {
+         int a;
+         a = 3;
+
+         System.out.println("Regeln fuer den Erfolg:");
+         System.out.println();
+
+         for(int i = 1; i <= a; i++)
+         {
+            System.out.println("Regel " + i + ": ");
+            System.out.println("Ueben!");
+         }
+
+      }
+}
