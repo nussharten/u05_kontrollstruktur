567a9aab247a4d329bd028eb446092f40703af8e
André Marín <andre.m@game-matter.com>
2025-10-08 14:52:23 +0200

    Update Versteigerung.java

--- auf5.09/Versteigerung.java
+++ auf5.09/Versteigerung.java
@@ -3,15 +3,15 @@
 
 public class Versteigerung
 {
-   public static void main(String[] args)
-      {
-			int a;
-			a = 500;
+	public static void main(String[] args)
+	{
+		int a;
+		a = 500;
 
-			while(a < 1000)
-			{
-				System.out.println(a + " Euro");
-				a = a + 100;
-			}
+		while(a < 1000)
+		{
+			System.out.println(a + " Euro");
+			a = a + 100;
 		}
+	}
 }
