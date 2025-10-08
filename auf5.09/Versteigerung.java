a6bb0e101ff8861e1ab0317c1d903bf41a0ad18a
Oleksandr Bilousov <bilousov757@gmail.com>
2025-10-08 14:46:46 +0200

    Create new file Versteigerung.java

---
+++ auf5.09/Versteigerung.java
@@ -0,0 +1,17 @@
+// u05_kontrollstruktur
+// auf5.09
+
+public class Versteigerung
+{
+   public static void main(String[] args)
+      {
+			int a;
+			a = 500;
+
+			while(a < 1000)
+			{
+				System.out.println(a + " Euro");
+				a = a + 100;
+			}
+		}
+}
