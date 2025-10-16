026a5fa88a72691d91195ff1dd822cd4e5ba5e1d
Oleksandr Bilousov <bilousov757@gmail.com>
2025-10-16 12:41:57 +0200

    Create new file BreakEventPoint2.java

---
+++ auf5.28/BreakEvenPoint2.java
@@ -0,0 +1,49 @@
+// u05_kontrollstruktur
+// auf5.28
+
+
+public class BreakEvenPoint2
+{
+   public static void main(String[] args)
+      {
+			int verkaufspreis  = 1500;
+			int rohstoffkosten = 500;
+			int arbeitskosten  = 300;
+			int fixkosten      = 5000;
+    		int maxStueckzahl  = 15;
+
+			System.out.println ();
+			System.out.println ("Break-Event-Point");
+			System.out.println ("=================");
+
+			System.out.println ("Verkaufspreis pro Stueck:  " + verkaufspreis);
+			System.out.println ("Rohstoffkosten pro Stueck: " + rohstoffkosten);
+			System.out.println ("Arbeitskosten pro Stueck:  " + arbeitskosten);
+			System.out.println ("Fixkosten pro Monat:       " + fixkosten);
+			System.out.println ("Maximale Stueckzahl pro Monat: " + maxStueckzahl);
+
+			int breakEventstueck = 0;
+
+			for (int i = 1; i <= maxStueckzahl; i++)
+			{
+				int variableKosten = (rohstoffkosten + arbeitskosten) * i;
+				int gesamtkosten = fixkosten + variableKosten;
+				int erloes = verkaufspreis * i;
+				int ueberschuss = erloes - gesamtkosten;
+ 
+				System.out.println();
+				System.out.printf("Stueckzahl:%3d  Gesamtkosten:%6d  Erloes:%6d  Ueberschuss:%7d", i, gesamtkosten, erloes, ueberschuss);
+
+				if(breakEventstueck == 0 && ueberschuss >= 0)
+				{
+					breakEventstueck = i;
+				}
+			 }
+					System.out.println ();
+					System.out.println ();
+				
+					{
+						System.out.println("Es müssen mindestens " + breakEventstueck + " Stück produziert werden.");
+					}
+		  }
+}
